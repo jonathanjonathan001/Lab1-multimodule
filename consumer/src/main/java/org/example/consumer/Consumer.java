49c1a14b814b30033fdc2a0f191aad8ea23d5e9f
Jonathan Råsmark <jonathanrosemark@gmail.com>
2023-04-20 14:34:46 +0200

    Delete annotations

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -1,79 +1,19 @@
 package org.example.consumer;
 
 
-import org.example.service.annotation.Language;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Set;
-import java.util.stream.Collectors;
+import org.example.service.Greeting;
+import java.util.ServiceLoader;
 
 public class Consumer {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, IOException, NoSuchMethodException, InstantiationException {
+    public static void main(String[] args)  {
 
-//        ServiceLoader<Greeting> greetings = ServiceLoader.load(Greeting.class);
-//
-//        for (var greeting : greetings) {
-//            System.out.println(greeting.sayHello());
-//        }
+        ServiceLoader<Greeting> greetings = ServiceLoader.load(Greeting.class);
 
-        // Leta efter klasser i paketet org.example.provider
-        Set<Class> classes = findAllClasses("org.example.provider");
-
-        // Kolla om klassen har annotationen @Language
-        for (var c : classes) {
-            var annotation = (Language) c.getAnnotation(Language.class);
-            if (annotation != null) {
-                // Anropa klassens metod och skriv ut returvärdet
-                System.out.println(annotation.value());
-                var o = c.getConstructor().newInstance();
-                var methods = c.getMethods();
-                for (var m : methods) {
-                    if (m.getReturnType().equals(String.class) && m.getParameterCount() == 0
-                    && !m.getName().equals("toString")) {
-                        var s = (String) m.invoke(o);
-                        System.out.println(s);
-
-                    }
-                }
-            }
-
+        for (var greeting : greetings) {
+            System.out.println(greeting.sayHello());
         }
 
 
-
-
-
     }
-
-    private static Set<Class> findAllClasses(String packageName) throws IOException {
-
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toSet());
-    }
-
-    private static Class getClass(String className, String packageName) {
-        try {
-            return Class.forName(packageName + "."
-                    + className.substring(0, className.lastIndexOf('.')));
-        } catch (ClassNotFoundException e) {
-            // handle the exception
-        }
-        return null;
-
-
-    }
-
-
-
-
 
 }
